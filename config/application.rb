867e37bbc9cb0f195ee6dcde9c2cb67b2f4e89a9
Kensuke Nagae <kyanny@gmail.com>
2014-04-06 01:30:17 +0900

    Don't load active_record

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,17 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+%w(
+  action_controller
+  action_view
+  action_mailer
+  rails/test_unit
+  sprockets
+).each do |framework|
+  begin
+    require "#{framework}/railtie"
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
