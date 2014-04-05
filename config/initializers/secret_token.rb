b484781420ca273367323fc0f83ed1041a097e36
Kensuke Nagae <kyanny@gmail.com>
2014-04-06 01:24:09 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GistFeed::Application.config.secret_key_base = 'f45b2d4e66ff9e31425247bce75eafda81bc9435a4652580970ca66e537fad0ff2fa16bb6ca3eb41ef273928958c12b1bee8b21c58a71db93d5106eb729e3c80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GistFeed::Application.config.session_store :cookie_store, key: '_gist_feed_session'
